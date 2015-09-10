0bba1c36bbf350f695472d8233e82ff3d3b2f4e6
Matthew Vernacchia <mvernacc@mit.edu>
2015-09-10 11:23:46 -0400

    Add gnome-i3 window manager configs.

---
+++ gnome-i3/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Setup the i3 window manager over a gnome session.
+# Reference: http://blog.hugochinchilla.net/2013/03/using-gnome-3-with-i3-window-manager/
+cp gnome-i3.desktop /usr/share/xsessions
+cp i3.session /usr/share/gnome-session/sessions
+
