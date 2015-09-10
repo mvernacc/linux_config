8d8e641a5bd746d063ac63ab7f7d4a3493207e86
Matthew Vernacchia <mvernacc@mit.edu>
2015-09-10 12:07:29 -0400

    Gnome-i3: add sudo to cp, remove bottom panel from gnome.

--- gnome-i3/install.sh
+++ gnome-i3/install.sh
@@ -2,6 +2,9 @@
 
 # Setup the i3 window manager over a gnome session.
 # Reference: http://blog.hugochinchilla.net/2013/03/using-gnome-3-with-i3-window-manager/
-cp gnome-i3.desktop /usr/share/xsessions
-cp i3.session /usr/share/gnome-session/sessions
+sudo cp gnome-i3.desktop /usr/share/xsessions
+sudo cp i3.session /usr/share/gnome-session/sessions
+cp config ~/.i3
 
+# Remove the bottom panel/bar from gnome.
+gsettings set org.gnome.gnome-panel.layout toplevel-id-list "['top-panel-0']"
