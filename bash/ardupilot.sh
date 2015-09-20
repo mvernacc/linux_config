1dcfeeeb24d1a9b4108097282dfbe91c819b491b
Matthew Vernacchia <mvernacc@mit.edu>
2015-09-20 12:35:33 -0400

    Add ardupilot stuff

---
+++ bash/ardupilot.sh
@@ -0,0 +1,5 @@
+# Ardupilot SITL setup from http://dev.ardupilot.com/wiki/setting-up-sitl-on-linux/#add_some_directories_to_your_search_path.
+
+export PATH=$PATH:$HOME/jsbsim/src
+export PATH=$PATH:$HOME/ardupilot/Tools/autotest 
+export PATH=/usr/lib/ccache:$PATH
